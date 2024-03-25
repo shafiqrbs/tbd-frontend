b2e1066569bbc9e9ecfe70165e4dc0a40c9edd53
Shafiq Isalm <shafiqabs@gmail.com>
2024-03-26 00:53:24 +0600

    Add Purchase module draft.

---
+++ src/components/modules/inventory/purchase/SalesIndex.jsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from "react";
+import {
+    Box, Button,
+    Grid, Progress, Title
+} from "@mantine/core";
+import {useTranslation} from 'react-i18next';
+import {useDispatch, useSelector} from "react-redux";
+import genericCss from '../../../../assets/css/Generic.module.css';
+
+import SalesTable from "./SalesTable";
+import PurchaseForm from "./PurchaseForm";
+import SalesUpdateForm from "./SalesUpdateForm.jsx";
+import {
+    setCustomerFilterData,
+    setInsertType,
+    setSearchKeyword,
+    setVendorFilterData
+} from "../../../../store/core/crudSlice.js";
+import {getShowEntityData} from "../../../../store/inventory/crudSlice.js";
+import GeneralPurchaseForm from "./GeneralPurchaseForm";
+
+function SalesIndex() {
+    const {t, i18n} = useTranslation();
+    const dispatch = useDispatch();
+    const [progress, setProgress] = useState(0);
+    const insertType = useSelector((state) => state.crudSlice.insertType)
+    useEffect(() => {
+        const updateProgress = () => setProgress((oldProgress) => {
+            if (oldProgress === 100) return 100;
+            const diff = Math.random() * 20;
+            return Math.min(oldProgress + diff, 100);
+        });
+        const timer = setInterval(updateProgress, 100);
+        return () => clearInterval(timer);
+    }, []);
+
+    const configData = useSelector((state) => state.inventoryCrudSlice.showEntityData)
+
+    useEffect(() => {
+        dispatch(getShowEntityData('inventory/config'))
+    }, []);
+
+
+    return (
+        <>
+            {progress !== 100 &&
+                <Progress color="red" size={"xs"} striped animated value={progress} transitionDuration={200}/>}
+            {progress === 100 &&
+                <Box>
+                    <Box pl={`md`} pr={8} pb={'8'} pt={'6'}  className={genericCss.titleBackground} >
+                        <Grid>
+                            <Grid.Col span={12}>
+                                <Title order={6} pl={'md'} fz={'18'} c={'black'}>{t('SalesInformation')}</Title>
+                            </Grid.Col>
+                        </Grid>
+                    </Box>
+                    <Box pr={'12'} pl={'12'}>
+                        {
+                            insertType === 'create' && configData.business_model.slug==='general' &&
+                            <GeneralPurchaseForm
+                                allowZeroPercentage = {configData.zero_stock}
+                                currancySymbol = {configData.currency.symbol}
+                            />
+                        }
+                    </Box>
+                </Box>
+            }
+        </>
+    );
+}
+
+export default SalesIndex;
