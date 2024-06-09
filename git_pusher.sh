6cbafada6c549ddd738df301d50ea053da53a7b2
Right_Brain_Raju <raju.rightbrainsolution@gmail.com>
2024-06-09 10:35:25 +0600

    layout backup before timeout fun added

--- git_pusher.sh
+++ git_pusher.sh
@@ -1,5 +1,5 @@
 git status
 git add .
-git commit -m "opening stock create , index & approve complete"
+git commit -m "layout backup before timeout fun added"
 git pull origin main
 git push origin main
---
+++ src/components/layout/Layout-bk.jsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from "react";
+import {Navigate, Outlet, useLocation} from "react-router-dom";
+import Header from "./Header";
+import {AppShell} from "@mantine/core";
+import {useDisclosure, useViewportSize} from "@mantine/hooks";
+import Footer from "./Footer";
+import MainDashboard from "../modules/dashboard/MainDashboard";
+
+
+function Layout() {
+    const [mobileOpened, {toggle: toggleMobile}] = useDisclosure(false);
+    const [navbarOpened, {toggle: toggleNavbar}] = useDisclosure(true);
+    const [rightSidebarOpened, {toggle: toggleRightSideBar}] = useDisclosure(false);
+    const {height, width} = useViewportSize();
+    const [isOnline, setNetworkStatus] = useState(navigator.onLine);
+
+    const user = localStorage.getItem("user");
+    const location = useLocation();
+    const paramPath = window.location.pathname;
+    if(!user){
+        return <Navigate replace to="/login"/>;
+    }
+
+    useEffect(() => {
+        return () => {
+            window.addEventListener("online", () => setNetworkStatus(true));
+            window.addEventListener("offline", () => setNetworkStatus(false));
+        };
+    }, []);
+
+    const headerHeight = 42;
+    const footerHeight = 36;
+    const mainAreaHeight = height - (headerHeight + footerHeight + 16); //default padding 20
+
+    return (
+        <>
+            <AppShell
+                header={{height: headerHeight}}
+                footer={{height: footerHeight}}
+                padding="0"
+            >
+                <AppShell.Header bg={`gray.0`}>
+                    <Header
+                        isOnline={isOnline}
+                        navbarOpened={navbarOpened}
+                        toggleNavbar={toggleNavbar}
+                        rightSidebarOpened={rightSidebarOpened}
+                        toggleRightSideBar={toggleRightSideBar}
+                    />
+                </AppShell.Header>
+
+                <AppShell.Main>
+                    {
+                        paramPath !== '/' ?
+                            <Outlet context={{isOnline, mainAreaHeight}}/>
+                            :
+                            <MainDashboard height={mainAreaHeight} />
+                    }
+
+                </AppShell.Main>
+
+                <AppShell.Footer>
+                    <Footer/>
+                </AppShell.Footer>
+            </AppShell>
+        </>
+    );
+}
+
+export default Layout;
