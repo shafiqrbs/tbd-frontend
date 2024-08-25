93ef19a7942547bc2830cd5c5eabc21f560732f1
foysal-mahmud-hasan <1730150@iub.edu.bd>
2024-08-25 15:35:36 +0600

    loader fix

--- src/components/modules/inventory/category-group/CategoryGroupIndex.jsx
+++ src/components/modules/inventory/category-group/CategoryGroupIndex.jsx
@@ -45,7 +45,7 @@ function CategoryGroupIndex() {
     return (
         <>
             {progress !== 100 &&
-                <Progress color="red" size={"xs"} striped animated value={progress} transitionDuration={200} />}
+                <Progress color="red" size={"sm"} striped animated value={progress} transitionDuration={200} />}
             {progress === 100 &&
                 <Box>
                     {configData &&
