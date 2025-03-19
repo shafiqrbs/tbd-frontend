51329f411cd410e416463bdb81ca021bd1858c00
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-03-19 14:54:44 +0600

    invoice batch icon update and code cleanup

--- src/components/advance-search/SearchActionMenu.jsx
+++ src/components/advance-search/SearchActionMenu.jsx
@@ -1,56 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import React, {  useState } from "react";
 import {
-    rem,
-    Grid, Tooltip, TextInput, ActionIcon, Select, Button, Flex, Box
+    rem, Tooltip, ActionIcon,  Box
 } from "@mantine/core";
 import { useTranslation } from 'react-i18next';
-import {
-    IconBrandOkRu, IconFileTypeXls,
-    IconFilter,
-    IconInfoCircle, IconPdf,
+import { IconFileTypeXls,
+    IconFilter, IconPdf,
     IconRestore,
     IconSearch,
-    IconX,
 } from "@tabler/icons-react";
 import { useHotkeys } from "@mantine/hooks";
 import { useDispatch, useSelector } from "react-redux";
-import { setSearchKeyword } from "../../store/core/crudSlice.js";
-import FilterModel from "../../components/modules/filter/FilterModel.jsx";
-import { setFetching, setInvoiceBatchFilterData, storeEntityData } from "../../store/inventory/crudSlice.js";
+import { setFetching, setInvoiceBatchFilterData } from "../../store/inventory/crudSlice.js";
 import _FilterSearch from '../../components/modules/inventory/invoice-batch/drawer/_FilterSearch.jsx'
 
 function SearchActionMenu(props) {
     const { t, i18n } = useTranslation();
     const dispatch = useDispatch();
-    const navigate = useNavigate()
-    const { isOnline } = useOutletContext();
 
     const [searchKeywordTooltip, setSearchKeywordTooltip] = useState(false)
     const [filterModel, setFilterModel] = useState(false)
 
     const invoiceBatchFilterData = useSelector((state) => state.inventoryCrudSlice.invoiceBatchFilterData)
 
-    /*START GET CUSTOMER DROPDOWN FROM LOCAL STORAGE*/
-    const [customersDropdownData, setCustomersDropdownData] = useState([])
-    const [refreshCustomerDropdown, setRefreshCustomerDropdown] = useState(false)
-
-    useEffect(() => {
-        let coreCustomers = localStorage.getItem('core-customers');
-        coreCustomers = coreCustomers ? JSON.parse(coreCustomers) : []
-        if (coreCustomers && coreCustomers.length > 0) {
-            const transformedData = coreCustomers.map(type => {
-                return ({ 'label': type.mobile + ' -- ' + type.name, 'value': String(type.id) })
-            });
-            setCustomersDropdownData(transformedData);
-            setRefreshCustomerDropdown(false)
-        }
-    }, [refreshCustomerDropdown])
-    /*END GET CUSTOMER DROPDOWN FROM LOCAL STORAGE*/
-
-    let [resetKey, setResetKey] = useState(0);
-
-    const resetDropDownState = () => setResetKey(prevKey => prevKey + 1);
 
     useHotkeys(
         [['alt+F', () => {
--- src/components/modules/inventory/invoice-batch/_InvoiceBatchSearch.jsx
+++ src/components/modules/inventory/invoice-batch/_InvoiceBatchSearch.jsx
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import React from "react";
 import {
-    rem,
-    Grid, Tooltip, TextInput, ActionIcon, Select, Button, Flex, Box
+    Grid, Flex, Box
 } from "@mantine/core";
-import { useTranslation } from 'react-i18next';
-import {
-    IconBrandOkRu, IconFileTypeXls,
-    IconFilter,
-    IconInfoCircle, IconPdf,
-    IconRestore,
-    IconSearch,
-    IconX,
-} from "@tabler/icons-react";
-import { useHotkeys } from "@mantine/hooks";
-import { useDispatch, useSelector } from "react-redux";
-import { setSearchKeyword } from "../../../../store/core/crudSlice.js";
-import FilterModel from "../../filter/FilterModel.jsx";
-import { setFetching, setInvoiceBatchFilterData, storeEntityData } from "../../../../store/inventory/crudSlice.js";
-import { DateInput } from "@mantine/dates";
-import {
-    setCategoryGroupFilterData,
-    setCustomerFilterData,
-    setUserFilterData,
-    setVendorFilterData
-} from "../../../../store/core/crudSlice";
-import { setProductFilterData } from "../../../../store/inventory/crudSlice";
 import SearchKeyword from "../../../advance-search/SearchKeyword.jsx";
 import SearchChooseCustomer from "../../../../components/advance-search/SearchChooseCustomer.jsx";
 import SearchStartDate from "../../../advance-search/SearchStartDate.jsx";
