c9a2a486b80d5648a4a332dff7912a9a29ecb418
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-04-23 15:30:42 +0600

    inventory nvaigation shortcut added

--- src/components/modules/inventory/inventory-configuration/InventoryConfigurationIndex.jsx
+++ src/components/modules/inventory/inventory-configuration/InventoryConfigurationIndex.jsx
@@ -1,44 +1,46 @@
 import React from "react";
-import {
-    Box,
-    Grid, Progress,
-} from "@mantine/core";
-import { useTranslation } from 'react-i18next';
+import { Box, Grid, Progress } from "@mantine/core";
+import { useTranslation } from "react-i18next";
 import { getLoadingProgress } from "../../../global-hook/loading-progress/getLoadingProgress.js";
 import InventoryHeaderNavbar from "../../domain/configuraton/InventoryHeaderNavbar.jsx";
 import InventoryConfigurationForm from "./InventoryConfigurationForm.jsx";
 import getConfigData from "../../../global-hook/config-data/getConfigData.js";
+import Navigation from "../common/Navigation.jsx";
 
 function InventoryConfigurationIndex() {
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    const progress = getLoadingProgress()
-    const {configData,fetchData} = getConfigData()
-    localStorage.setItem('config-data', JSON.stringify(configData));
+  const progress = getLoadingProgress();
+  const { configData, fetchData } = getConfigData();
+  localStorage.setItem("config-data", JSON.stringify(configData));
 
-    return (
-
+  return (
+    <>
+      {progress !== 100 && (
+        <Progress color="red" size={"sm"} striped animated value={progress} />
+      )}
+      {progress === 100 && (
         <>
-            {progress !== 100 && <Progress color="red" size={"sm"} striped animated value={progress} />}
-            {progress === 100 &&
-                <>
-                    <InventoryHeaderNavbar
-                        pageTitle={t('ProductConfiguration')}
-                        roles={t('Roles')}
-                        allowZeroPercentage=''
-                        currencySymbol=''
-                    />
-                    <Box p={'8'}>
-                        <Grid columns={24} gutter={{ base: 8 }}>
-                            <Grid.Col span={24}>
-                                <InventoryConfigurationForm />
-                            </Grid.Col>
-                        </Grid>
-                    </Box>
-                </>
-            }
+          <InventoryHeaderNavbar
+            pageTitle={t("ProductConfiguration")}
+            roles={t("Roles")}
+            allowZeroPercentage=""
+            currencySymbol=""
+          />
+          <Box p={"8"}>
+            <Grid columns={24} gutter={{ base: 8 }}>
+              <Grid.Col span={1}>
+                <Navigation module={"config"} />
+              </Grid.Col>
+              <Grid.Col span={23}>
+                <InventoryConfigurationForm />
+              </Grid.Col>
+            </Grid>
+          </Box>
         </>
-    );
+      )}
+    </>
+  );
 }
 
 export default InventoryConfigurationIndex;
