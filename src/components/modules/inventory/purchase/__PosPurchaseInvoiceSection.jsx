3bd03e0f825b6efe7cdab8a48274595af1220a4e
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-04-24 17:00:34 +0600

    new sales & purchase routing issue for looped useeffct solve

--- src/components/modules/inventory/purchase/__PosPurchaseInvoiceSection.jsx
+++ src/components/modules/inventory/purchase/__PosPurchaseInvoiceSection.jsx
@@ -70,21 +70,6 @@ export default function __PosPurchaseInvoiceSection(props) {
     ? JSON.parse(localStorage.getItem("accounting-transaction-mode"))
     : [];
 
-  useEffect(() => {
-    if (transactionModeData && transactionModeData.length > 0) {
-      for (let mode of transactionModeData) {
-        if (mode.is_selected) {
-          form.setFieldValue(
-            "transaction_mode_id",
-            form.values.transaction_mode_id
-              ? form.values.transaction_mode_id
-              : mode.id
-          );
-          break;
-        }
-      }
-    }
-  }, [transactionModeData, form]);
   // transaction modes hover hook
   const [hoveredModeId, setHoveredModeId] = useState(false);
 
