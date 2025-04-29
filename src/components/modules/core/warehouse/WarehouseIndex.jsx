141d54e5ffc7ff384a5ee86b5117d6c79e9b3653
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-04-29 13:31:47 +0600

    update of prev commit

--- src/components/modules/core/warehouse/WarehouseIndex.jsx
+++ src/components/modules/core/warehouse/WarehouseIndex.jsx
@@ -5,11 +5,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
 import {
-    setEntityNewData,
-    setInsertType,
-    setSearchKeyword,
-    editEntityData,
-    setFormLoading
+  setEntityNewData,
+  setInsertType,
+  setSearchKeyword,
+  editEntityData,
+  setFormLoading,
 } from "../../../../store/core/crudSlice.js";
 
 import { getLoadingProgress } from "../../../global-hook/loading-progress/getLoadingProgress.js";
@@ -19,64 +19,72 @@ import CoreHeaderNavbar from "../CoreHeaderNavbar.jsx";
 import WarehouseTable from "./WarehouseTable.jsx";
 import WarehouseForm from "./WarehouseForm.jsx";
 import WarehouseUpdateForm from "./WarehouseUpdateForm.jsx";
+import Navigation from "../common/Navigation.jsx";
 
 function WarehouseIndex() {
-    const { t } = useTranslation();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { insertType } = useSelector((state) => state.crudSlice);
-    const progress = getLoadingProgress();
-    const { configData = {} } = getConfigData();
-    const { id } = useParams();
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { insertType } = useSelector((state) => state.crudSlice);
+  const progress = getLoadingProgress();
+  const { configData = {} } = getConfigData();
+  const { id } = useParams();
 
-    useEffect(() => {
-        if (id) {
-            dispatch(setInsertType("update"));
-            dispatch(editEntityData(`/core/warehouse/${id}`));
-            dispatch(setFormLoading(true));
-        } else {
-            dispatch(setInsertType("create"));
-            dispatch(setSearchKeyword(""));
-            dispatch(setEntityNewData([]));
+  useEffect(() => {
+    if (id) {
+      dispatch(setInsertType("update"));
+      dispatch(editEntityData(`/core/warehouse/${id}`));
+      dispatch(setFormLoading(true));
+    } else {
+      dispatch(setInsertType("create"));
+      dispatch(setSearchKeyword(""));
+      dispatch(setEntityNewData([]));
 
-            if (window.location.pathname !== "/core/warehouse") {
-                navigate("/core/warehouse");
-            }
-        }
-    }, [id]);
+      if (window.location.pathname !== "/core/warehouse") {
+        navigate("/core/warehouse");
+      }
+    }
+  }, [id]);
 
-    return (
-        <>
-            {progress < 100 && (
-                <Progress
-                    color="red"
-                    size="sm"
-                    striped
-                    animated
-                    value={progress}
-                    transitionDuration={200}
-                />
-            )}
+  return (
+    <>
+      {progress < 100 && (
+        <Progress
+          color="red"
+          size="sm"
+          striped
+          animated
+          value={progress}
+          transitionDuration={200}
+        />
+      )}
 
-            {progress === 100 && configData && (
-                <Box>
-                    <CoreHeaderNavbar pageTitle={t("Warehouse")} roles={t("Roles")} />
-                    <Box p="8">
-                        <Grid columns={24} gutter={{ base: 8 }}>
-                            <Grid.Col span={15}>
-                                <Box bg="white" p="xs" className="borderRadiusAll">
-                                    <WarehouseTable />
-                                </Box>
-                            </Grid.Col>
-                            <Grid.Col span={9}>
-                                {insertType === "create" ? <WarehouseForm /> : <WarehouseUpdateForm />}
-                            </Grid.Col>
-                        </Grid>
-                    </Box>
+      {progress === 100 && configData && (
+        <Box>
+          <CoreHeaderNavbar pageTitle={t("Warehouse")} roles={t("Roles")} />
+          <Box p="8">
+            <Grid columns={24} gutter={{ base: 8 }}>
+              <Grid.Col span={1}>
+                <Navigation module={"warehouse"} />
+              </Grid.Col>
+              <Grid.Col span={14}>
+                <Box bg="white" p="xs" className="borderRadiusAll">
+                  <WarehouseTable />
                 </Box>
-            )}
-        </>
-    );
+              </Grid.Col>
+              <Grid.Col span={9}>
+                {insertType === "create" ? (
+                  <WarehouseForm />
+                ) : (
+                  <WarehouseUpdateForm />
+                )}
+              </Grid.Col>
+            </Grid>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 }
 
 export default WarehouseIndex;
