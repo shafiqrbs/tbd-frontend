27cfea14178e89ed9fa98ba40e18dba02b42cb8d
foysal-mahmud-hasan <1730150@iub.edu.bd>
2025-04-29 13:16:40 +0600

    update

--- src/components/modules/inventory/purchase/_PurchaseTable.jsx
+++ src/components/modules/inventory/purchase/_PurchaseTable.jsx
@@ -259,7 +259,7 @@ function _PurchaseTable() {
             <Box>
                 <Grid columns={24} gutter={{base: 8}}>
                     <Grid.Col span={1}>
-                        <Navigation module = "sales" />
+                        <Navigation module = {"sales"} />
                     </Grid.Col>
                     <Grid.Col span={14}>
                         <Box bg={"white"} p={"xs"} className={"borderRadiusAll"}>
