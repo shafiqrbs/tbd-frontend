0b6c69493baadf4427c0cc5c38050bb83c304590
Md Shafiqul Islam <55863127+shafiqrbs@users.noreply.github.com>
2025-05-14 13:02:15 +0600

    Update login after reload

--- src/components/modules/dashboard/MainDashboard.jsx
+++ src/components/modules/dashboard/MainDashboard.jsx
@@ -85,13 +85,14 @@ function MainDashboard(props) {
         const checkDomainConfig = () => {
             const domainConfigData = localStorage.getItem("domain-config-data");
             const inventoryConfig = JSON.parse(domainConfigData || '{}')?.inventory_config;
-            
+
             if (!inventoryConfig?.id) {
+                console.log("redirect to login from MainDashboard");
                 navigate("/login");
             }
         };
-
-        setTimeout(checkDomainConfig, 500);
+        const timeoutId = setTimeout(checkDomainConfig, 500);
+        return () => clearTimeout(timeoutId);
     }, [navigate]);
 
     return (
